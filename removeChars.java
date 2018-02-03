4d6e2c731b664005a14bc0f560e0cdc7a6ce604a
Eduardo Luna <lalo.lunagt@gmail.com>
2018-02-03 09:42:18 -0600

    removeChars

---
+++ removeChars.java
@@ -0,0 +1,17 @@
+/* Crea un metodo que reciva 2 char[] A y B, el A  es una palabra cualquiera
+y el B es un conjunto de caracteres que tienes que remover de A.
+Tienes que retornar la primera palabra A con puntos en los lugares en los que habia caracteres contenidos en B.
+
+ejem:
+
+A = programacion
+B = arn
+
+retornar = p.og..m.cio.
+*/
+
+public class removeChars{
+	public static void main(String [] args){
+
+	}
+} 
